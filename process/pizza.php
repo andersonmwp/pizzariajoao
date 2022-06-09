613befbdb3796281c7790bb614fa46504dbf0771
Anderson Moura <andersonmwp@gmail.com>
2022-06-08 23:14:17 -0300

    finalizando o envio dos pedidos na home

--- process/pizza.php
+++ process/pizza.php
@@ -22,6 +22,64 @@
     // Enviando dados, criação de pedidos
     } else if($method === "POST") {
 
+        $data = $_POST;
+
+        $borda = $data["borda"];
+        $massa = $data["massa"];
+        $sabores = $data["sabores"];
+
+        // Validação de sabores máximos
+        if(count($sabores) > 3){
+
+            $_SESSION["msg"] = "Selecione no máximo 3 sabores!";
+            $_SESSION["status"] = "warning";
+
+        } else {
+
+            // Salvando borda e massa na pizza
+            $stmt = $conn->prepare("INSERT INTO pizzas (borda_id, massa_id) VALUES (:borda, :massa)");
+
+            // Filtrando inputs
+            $stmt->bindParam(":borda", $borda, PDO::PARAM_INT);
+            $stmt->bindParam(":massa", $massa, PDO::PARAM_INT);
+
+            $stmt->execute();
+
+            // Resgatando id da última pizza
+            $pizzaId = $conn->lastInsertId();
+
+            $stmt = $conn->prepare("INSERT INTO pizza_sabor (pizza_id, sabor_id) VALUES (:pizza, :sabor)");
+
+            //Repetição até terminar todos os sabores
+            foreach($sabores as $sabor) {
+
+                // Filtrando os inputs
+                $stmt->bindParam(":pizza", $pizzaId, PDO::PARAM_INT);
+                $stmt->bindParam(":sabor", $sabor, PDO::PARAM_INT);
+
+                $stmt->execute();
+            }
+
+            // Criando o pedido da pizza
+            $stmt = $conn->prepare("INSERT INTO pedidos (pizza_id, status_id) VALUES (:pizza, :status)");
+
+            // Status sempre inicia com 1, que é em produção
+            $statusId = 1;
+
+            // Filtrando inputs
+            $stmt->bindParam(":pizza", $pizzaId);
+            $stmt->bindParam(":status", $statusId);
+
+            $stmt->execute();
+
+            // Exibir mensagem de sucesso
+            $_SESSION["msg"] = "Pedido realizado com sucesso!";
+            $_SESSION["status"] = "success";
+
+        }
+
+        // Retorna para a página inicial
+        header("Location: ..");
     }
 
 ?>
--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,20 @@
+<?php
+
+    include("process/conn.php");
+
+    $msg = "";
+
+    if (isset($_SESSION["msg"])) {
+
+        $msg = $_SESSION["msg"];
+        $status = $_SESSION["status"];
+
+        $_SESSION["msg"] = "";
+        $_SESSION["status"] = "";
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,6 +44,9 @@
             </div>
         </nav>
     </header>
-    <div class="alert alert-success">
-        <p>Pedido feito com sucesso!</p>
-    </div>
+    <?php if($msg != ""): ?>
+        <div class="alert alert-<? $status ?>">
+            <p><?= $msg ?></p>
+        </div>
+    <?php endif; ?>
+
