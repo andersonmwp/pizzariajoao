f6a0f7af836b4d6926b326947f4b583aee56b5fb
Anderson Moura <andersonmwp@gmail.com>
2022-06-08 22:21:51 -0300

    resgatando os dados no bd

--- process/pizza.php
+++ process/pizza.php
@@ -2,4 +2,26 @@
 
     include_once("conn.php");
 
+    $method = $_SERVER["REQUEST_METHOD"];
+
+    // Resgatando os dados, montagem de pedidos
+    if($method === "GET") {
+
+        $bordasQuery = $conn->query("SELECT * FROM bordas;");
+
+        $bordas = $bordasQuery->fetchAll();
+
+        $massasQuery = $conn->query("SELECT * FROM massas;");
+
+        $massas = $massasQuery->fetchAll();
+
+        $saboresQuery = $conn->query("SELECT * FROM sabores;");
+
+        $sabores = $saboresQuery->fetchAll();
+
+    // Enviando dados, criação de pedidos
+    } else if($method === "POST") {
+
+    }
+
 ?>
